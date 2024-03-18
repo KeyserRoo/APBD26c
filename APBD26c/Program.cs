6f0101de978b6cf35ed272c80414f78debc53c10
KeyserRoo <s28970@pjwstk.edu.pl>
2024-03-18 20:14:05 +0100

    max

--- APBD26c/Program.cs
+++ APBD26c/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+int GetMax(int[] ints)
+{
+  int max = 0;
+  foreach (int item in ints)
+  {
+    if (max < item) max = item;
+  }
+  return max;
+}
